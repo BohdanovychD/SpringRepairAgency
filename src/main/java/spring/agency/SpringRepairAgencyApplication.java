6be29ca62dd10199937c2cd9eb359536d87a6934
BohdanovychD <dima.bogdanovych2001@gmail.com>
2021-08-01 12:40:47 +0300

    Gitignore and maven files added

---
+++ src/main/java/spring/agency/SpringRepairAgencyApplication.java
@@ -0,0 +1,13 @@
+package spring.agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepairAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRepairAgencyApplication.class, args);
+    }
+
+}
